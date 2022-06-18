45879416b01276aaa00181d26a6feb156ebd493e
anjunhyuk <ajh941500@gmail.com>
2022-06-19 01:09:37 +0900

    gitignore 등록

--- src/main/java/webService2/springboot/domain/posts/Posts.java
+++ src/main/java/webService2/springboot/domain/posts/Posts.java
@@ -3,6 +3,7 @@ package webService2.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import webService2.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter             //  클래스 내 모든 필드의 Getter메소드를 자동생성
 @NoArgsConstructor  //  기본 생성자 자동 추가
 @Entity             //  테이블과 링크될 클래스임을 나타낸다.
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id // 해당테이블의 PK를 타나낸다. GenerationType.IDENTITY를 함으로써 auto imcrement가 된다.
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/webService2/springboot/serviece/posts/PostsService.java
+++ src/main/java/webService2/springboot/serviece/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import webService2.springboot.domain.posts.Posts;
 import webService2.springboot.domain.posts.PostsRepository;
+import webService2.springboot.web.dto.PostsListResponseDto;
 import webService2.springboot.web.dto.PostsResponseDto;
 import webService2.springboot.web.dto.PostsSaveRequestDto;
 import webService2.springboot.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,4 +40,15 @@ public class PostsService{
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+
+        postsRepository.delete(posts);
+    }
 }
